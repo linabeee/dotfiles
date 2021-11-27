7d794ff89fb407fc3cfc4c61c9b882d9ae7aa300
Lina <36717206+linabeee@users.noreply.github.com>
2021-11-27 19:03:39 +0000

    fix discord unread badge on nixos

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,6 +1,13 @@
 {
   allowUnfree = true;
-  packageOverrides = pkgs: {
-    neovim = pkgs.neovim.overrideAttrs (_: { wrapRc = false; });
-  };
+  packageOverrides = pkgs:
+    with pkgs; {
+      neovim = neovim.overrideAttrs (_: { wrapRc = false; });
+      discord = discord.override {
+        lib = lib // {
+          makeLibraryPath = xs: lib.makeLibraryPath (xs ++ [ libunity ]);
+        };
+      };
+    };
+  permittedInsecurePackages = [ "electron-9.4.4" ];
 }
