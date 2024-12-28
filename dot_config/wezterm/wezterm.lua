7007c6c3ff219a98f78e955e2fcbcd529151bf0c
Lina <36717206+lina-bh@users.noreply.github.com>
2024-12-28 09:44:52 +0000

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,43 +1,7 @@
 local wezterm = require 'wezterm'
 
--- Equivalent to POSIX basename(3)
--- Given "/foo/bar" returns "bar"
--- Given "c:\\foo\\bar" returns "bar"
--- function basename(s)
---     return string.gsub(s, '(.*[/\\])(.*)', '%2')
--- end
-
--- wezterm.on(
---     'format-tab-title',
---     function(tab, tabs, panes, config, hover, max_width)
---         local pane = tab.active_pane
---         local computed_title = string.format("%d: %s", pane.pane_id + 1, basename(pane.foreground_process_name))
---         return {
---             { Text = ' ' .. computed_title .. ' ' },
---         }
---     end
--- )
-
--- wezterm.on(
---     'format-window-title',
---     function(tab, pane, tabs, panes, config)
---         local pane = tab.active_pane
---         return string.format(
---             "@%s [%d/%d] %s",
---             wezterm.hostname(),
---             tab.tab_index + 1,
---             #tabs,
---             basename(pane.foreground_process_name)
---         )
---     end
--- )
-
 config = wezterm.config_builder()
-config.font = wezterm.font_with_fallback {
-  "Iosevka Nerd Font",
-  "Iosevka Term",
-  "Monospace",
-}
+-- config.font = wezterm.font("Iosevka Term")
 config.font_size = 11.0
 config.window_padding = {
   top = 1,
@@ -60,26 +24,9 @@ config.background = {
 config.window_close_confirmation = "NeverPrompt"
 config.swallow_mouse_click_on_pane_focus = true
 config.window_frame = {
-  font = wezterm.font_with_fallback { "Inter Medium", "DejaVu Sans", "Sans" },
   font_size = 10,
 }
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
--- config.default_ssh_auth_sock = wezterm.home_dir .. "/.1password/agent.sock"
-config.ssh_domains = {
-  {
-    name = "libreelec",
-    remote_address = "rpi4",
-    username = "root",
-  }
-}
 
--- {
---     source = {File=wezterm.config_dir .. "/background.jpg"},
---     opacity = 0.7,
---     hsb = {
---         saturation = 0.4,
---         brightness = 0.3,
---     },
--- },
 return config
