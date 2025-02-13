12906ad25711fd7c07c03aa6db18ad18ec188835
Lina <36717206+lina-bh@users.noreply.github.com>
2025-02-13 17:58:48 +0000

    Update .config/fish/functions/fish_prompt.fish

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
     set -f user "(test -n "$SSH_CLIENT"; and printf '%s ' (prompt_login))(test -n "$CONTAINER_ID"; and printf "[%s]" "$CONTAINER_ID")"
-    printf '%s%s > ' $user (prompt_pwd)
+    printf '%s%s> ' $user (prompt_pwd)
 end
