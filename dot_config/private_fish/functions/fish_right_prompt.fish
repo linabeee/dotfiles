f2173e1c816b6af1b85c180c85d4fdd9fd432c5c
Lina <36717206+lina-bh@users.noreply.github.com>
2025-02-13 18:30:59 +0000

    Update .config/fish/functions/fish_right_prompt.fish

--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -1,5 +1,8 @@
 function fish_right_prompt
   set -l laststatus $status
+  if ! jobs -q
+    printf '%%%s ' (jobs -p | count)
+  end
   if test $laststatus -ne 0
     set_color red
     printf '%s ' $laststatus
