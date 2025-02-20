ad51ad1007a62248d8bdc3305b128b84674071f2
Lina <36717206+lina-bh@users.noreply.github.com>
2025-02-20 17:54:21 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,5 @@ set -g fish_color_command normal
 set -g fish_color_param normal
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    abbr juserctl='journalctl --user'
 end
