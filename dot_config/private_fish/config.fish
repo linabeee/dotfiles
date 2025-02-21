8e549e6f5d55944e7517962e2bdb0c08d4bd9978
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-02-21 16:57:17 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,4 +3,8 @@ set -g fish_color_param normal
 if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr juserctl='journalctl --user'
+
+    if command -q tailscale
+      tailscale completion fish | source
+    end
 end
